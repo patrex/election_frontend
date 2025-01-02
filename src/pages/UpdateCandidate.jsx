615ed13ab2b0703a0ab8dc80d5ab34c20a462b71
sokari <patsoks.sokari@gmail.com>
2025-01-02 11:09:00 +0100

    revamp styles for candidate update container -1

--- src/pages/UpdateCandidate.jsx
+++ src/pages/UpdateCandidate.jsx
@@ -127,81 +127,77 @@ function UpdateCandidate() {
 	return ( 
 		<>
 			<div className="candidate-update-form-container">
-				<form onSubmit={handleSubmit(onSubmit)}>
-						<div className="update-candidate-top">
-							<div className="update-candidate-left">
-								<div>
-									<label htmlFor="firstname" className="form-label">Firstname: </label>
-									<input type="text" 
-										id="firstname" 
-										aria-describedby="firstname"
-										name="firstname"
-										autoFocus
-										{...register('firstname', {required: "First name must be at least two characters"})}
-									/>
-								</div>
-								<div>
-									<label htmlFor="lastname" className="form-label">Lastname: </label>
-									<input type="text" 
-										id="lastname" 
-										aria-describedby="lastname"
-										name="lastname"
-										{...register('lastname', {required: "Lastname must be at least two characters"})}
-									/>
-								</div>
-								
-								<div>
-									<label>
-										Select position:
-										<select {...register('selectedPosition', {required: "Select a position"})}
-											className='form-select form-select-lg mb-3'
-										> 
-											<option value={position.position} selected>{position.position}</option>
-											{positions.length > 0 ? 
-												positions.filter(position => candidate.position != position._id).map((position) => (
-													<option key={position.position} value={position.position}>
-														{position.position}
-													</option>
-												))
-											: "no positions.."}
-										</select>
-									</label>
-								</div>
-
-								<div className="mb-3">
-									<textarea name="manifesto"
-										id="" rows="3" cols="55"
-										className='mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
-										{...register('manifesto')}
-									/>
-								</div>
+				<div className="update-candidate-top">
+					<div className="update-candidate-left">
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<div>
+								<label htmlFor="firstname" className="form-label">Firstname: </label>
+								<input type="text" 
+									id="firstname" 
+									aria-describedby="firstname"
+									name="firstname"
+									autoFocus
+									{...register('firstname', {required: "First name must be at least two characters"})}
+								/>
+							</div>
+							<div>
+								<label htmlFor="lastname" className="form-label">Lastname: </label>
+								<input type="text" 
+									id="lastname" 
+									aria-describedby="lastname"
+									name="lastname"
+									{...register('lastname', {required: "Lastname must be at least two characters"})}
+								/>
+							</div>
+							
+							<div>
+								<label>
+									Select position:
+									<select {...register('selectedPosition', {required: "Select a position"})}
+										className='form-select form-select-lg mb-3'
+									> 
+										<option value={position.position} selected>{position.position}</option>
+										{positions.length > 0 ? 
+											positions.filter(position => candidate.position != position._id).map((position) => (
+												<option key={position.position} value={position.position}>
+													{position.position}
+												</option>
+											))
+										: "no positions.."}
+									</select>
+								</label>
 							</div>
 
-							<div className="update-candidate-right">
-								{/*  candidate picture */}
-								<div className="w-full md:w-1/2 flex justify-center">
-									<div className="w-48 h-48 rounded-full overflow-hidden border border-gray-300">
-										<img src={image} name="candidateimgUrl" className="w-full h-full object-cover" />
-									</div>
-									<div>
-										<input className='fileupload form-control-file' 
-											type="file"
-											id="fileuploadr" 
-											style={{ display: 'none' }}
-											{...register("imgUrl", {required: "Choose a picture"}) }
-										/>
-										<label htmlFor="fileuploadr" className="Button violet" style={{cursor: 'pointer', margin: "0.5rem 0rem"}}>Choose different picture</label>
-									</div> 
-									
-								</div>
+							<div className="mb-3">
+								<textarea name="manifesto"
+									id="" rows="3" cols="55"
+									className='mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+									{...register('manifesto')}
+								/>
 							</div>
+						</form>
+					</div>
+				
+
+					<div className="update-candidate-right">
+						<div className="candidate-update-pic-holder">
+							<img src={image} name="candidateimgUrl" className="" />
 						</div>
+						<div>
+							<input className='fileupload form-control-file' 
+								type="file"
+								id="fileuploadr" 
+								style={{ display: 'none' }}
+								onChange={ (e) => {setImage(e.target.files[0])} }
+							/>
+							<label htmlFor="fileuploadr" className="Button violet" style={{cursor: 'pointer', margin: "0.5rem 0rem"}}>Choose different picture</label>
+						</div> 
+					</div>
+				</div>
 						
-						
-						<div className="my-2 candidate-update-bottom">
-							<input type="submit" className="Button violet" value={"Save"} />
-						</div>
-				</form>
+				<div className="my-2 candidate-update-bottom">
+					<input type="submit" className="Button violet" value={"Save"} />
+				</div>
 			</div>
 		</>
 	);
