a619c436738cc92d34c9fb2c12c05595070c73e4
sokari <patsoks.sokari@gmail.com>
2025-02-05 23:14:56 +0100

    use component for row

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import ErrorBoundary from './pages/ErrorBoundary.jsx'
+import Error from './pages/Error.jsx'
+
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-	<App />
+	<ErrorBoundary fallback={Error}>
+		<App />
+	</ErrorBoundary>
   </React.StrictMode>,
 )
