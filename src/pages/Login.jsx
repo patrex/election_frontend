e6c0281bdd9f25ac89dbceb0a1655a5f0fa49d7d
sokari <patsoks.sokari@gmail.com>
2025-02-09 13:19:37 +0100

    refactor login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -93,7 +93,7 @@ function Login() {
 						</div>
 						
 						<button 
-							className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
+							className="w-[50px] md:w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
 							disabled={loading}
 						>{ loading ? <PulseLoader  color="#ffb500" size={5} loading={loading}/> : "Login" }</button>
 
