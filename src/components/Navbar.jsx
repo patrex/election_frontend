ea835186905d2503587b56e009ed8500472bb6d1
sokari <patsoks.sokari@gmail.com>
2025-02-16 16:29:54 +0100

    refactor nav again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ function NavBar({ user, onLogout}) {
 						<h2>Votify</h2>
 					</div>
 					<ul>
-						<li className="nav-link"><Link to={'/'} className="anchor-tag"></Link></li>
-						<li className="nav-link"><Link to={`/user/${user.uid}`} className="anchor-tag">Dashboard</Link></li>
-						<li className="nav-link"><Link to={`/user/${user.uid}/create-election`} className="anchor-tag">Create Election</Link></li>
+						<li className="nav-link"><a href="">Gello</a></li>
+						<li className="nav-link"><a href="">Dashboard</a></li>
+						<li className="nav-link"><a href="">Create Election</a></li>
 					</ul>
 				</nav>
 			</div>
