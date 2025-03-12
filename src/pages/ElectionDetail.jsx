49e4db6b3c7653da898771c3be530f62cb1b095a
sokari <patsoks.sokari@gmail.com>
2025-03-12 01:17:23 +0100

    refactor isactive and hasended

--- src/pages/ElectionDetail.jsx
+++ src/pages/ElectionDetail.jsx
@@ -477,9 +477,14 @@ function ElectionDetail() {
 						</tbody>
 					</table>
 					<div style={ {display: 'flex', justifyContent: 'flex-start'} }>
-						{!isActive && <p><button className='Button violet action-item' onClick={() => openPostionModal(election)}>Add Position</button></p>}
-						{!isActive && <p><Link to={`/user/${params.userId}/election/${election._id}/addcandidate`} className='Button violet action-item no-underline' onClick={checkPositionExists}>Add Candidate</Link></p>}
-						{!isActive && election.type === "Closed" && <p><button className='Button violet action-item' onClick={ () => setAddParticipantsModalOpen(true) }>Add Voters</button></p> }
+						{(!isActive && !hasEnded) && (
+							<>
+								<p><button className='Button violet action-item' onClick={() => openPostionModal(election)}>Add Position</button></p>
+								<p><Link to={`/user/${params.userId}/election/${election._id}/addcandidate`} className='Button violet action-item no-underline' onClick={checkPositionExists}>Add Candidate</Link></p>
+								{election.type === "Closed" && <p><button className='Button violet action-item' onClick={ () => setAddParticipantsModalOpen(true) }>Add Voters</button></p> }
+							
+							</>
+						)}
 						{election.type === "Closed" && <p><button className='Button violet action-item' onClick={ () => setViewUsersModal(true) }>View Voters</button></p> }
 						{isActive && <p><button className='Button red action-item' onClick={ () => setEndElectionModalOpen(true) }>End This Election!</button></p>}
 						{hasEnded && <p><Link to={`/election/${election._id}/results`} className='Button violet action-item no-underline'>View Results</Link></p>}
