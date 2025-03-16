a92b0c86d8744dd02b5504c8ce6cfd427fc1a720
sokari <patsoks.sokari@gmail.com>
2025-03-16 18:53:32 +0100

    modify login and signup buttons to fill div

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -122,7 +122,7 @@ function Login() {
 						</div>
 						
 						<button 
-							className="flex items-center justify-center block mx-auto px-4 py-2 mt-2 mb-2 bg-blue-500 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-500 transition duration-200"
+							className="w-full px-6 py-3 text-white bg-blue-600 rounded-lg shadow-md text-center transition-all duration-300 hover:bg-blue-700 hover:shadow-lg active:scale-95"
 							disabled={loading}
 						>{ loading ? <PulseLoader  color="#fff" size={5} loading={loading}/> : "Login" }</button>
 
