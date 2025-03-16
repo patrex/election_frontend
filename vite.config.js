627730a147d699cd86b0545b6e583da665c603df
sokari <patsoks.sokari@gmail.com>
2025-03-16 17:01:36 +0100

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import backendUrl from './src/utils/backendurl'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import polyfillNode from 'rollup-plugin-polyfill-node'
+
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   	plugins: [react()],
+	build: {
+		rollupOptions: {
+			plugins: [
+				resolve(),
+				commonjs(),
+				peerDepsExternal(),
+				polyfillNode()
+			]
+		}
+	},
   	resolve: {
 		alias: {
 	  		"@": path.resolve(__dirname, "./src"),
