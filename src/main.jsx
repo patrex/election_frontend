ca531bbe490c0a9fb8e11eabaa63eeeee5faf502
sokari <patsoks.sokari@gmail.com>
2025-03-18 14:45:28 +0100

    modify the package.json file

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import ErrorBoundary from './pages/ErrorBoundary.jsx'
-import Error from './pages/Error.jsx'
-
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-	<ErrorBoundary fallback={Error}>
 		<App />
-	</ErrorBoundary>
   </React.StrictMode>,
 )
