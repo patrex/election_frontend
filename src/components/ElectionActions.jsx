84e535b3b69997b073e1eae381f1d39fc93374de
sokari <patsoks.sokari@gmail.com>
2025-03-26 07:52:32 +0100

    refactor election actions

---
+++ src/components/ElectionActions.jsx
@@ -0,0 +1,56 @@
+import { Link } from 'react-router-dom';
+
+const ElectionActions = ({ election, isActive, hasEnded, openPostionModal, checkPositionExists, setAddParticipantsModalOpen, setViewUsersModal, setEndElectionModalOpen }) => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+      {(!isActive && !hasEnded) && (
+        <>
+          <p>
+            <button className='Button violet action-item' onClick={() => openPostionModal(election)}>
+              Add Position
+            </button>
+          </p>
+          <p>
+            <Link 
+              to={`/user/${params.userId}/election/${election._id}/addcandidate`} 
+              className='Button violet action-item no-underline' 
+              onClick={checkPositionExists}
+            >
+              Add Candidate
+            </Link>
+          </p>
+          {election.type === "Closed" && (
+            <p>
+              <button className='Button violet action-item' onClick={() => setAddParticipantsModalOpen(true)}>
+                Add Voters
+              </button>
+            </p>
+          )}
+        </>
+      )}
+      {election.type === "Closed" && (
+        <p>
+          <button className='Button violet action-item' onClick={() => setViewUsersModal(true)}>
+            View Voters
+          </button>
+        </p>
+      )}
+      {(isActive && !hasEnded) && (
+        <p>
+          <button className='Button red action-item' onClick={() => setEndElectionModalOpen(true)}>
+            End This Election!
+          </button>
+        </p>
+      )}
+      {hasEnded && (
+        <p>
+          <Link to={`/election/${election._id}/results`} className='Button violet action-item no-underline'>
+            View Results
+          </Link>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default ElectionActions;
