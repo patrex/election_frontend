7140d1846a9229b8ec2c8922c4fde2e3e9aae7b7
sokari <patsoks.sokari@gmail.com>
2025-03-31 18:09:54 +0100

    remove err image

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,9 @@
 import { useRouteError,
 	useNavigate,
-	useLocation
+	useLocation,
+	Link
 } from 'react-router-dom';
 
-import errImg from '../assets/error-image.png'
 
 
 function Error() {
@@ -14,15 +14,14 @@ function Error() {
 	return (  
 		<div class="flex items-center justify-center min-h-screen bg-gray-100">
 			<div class="text-center max-w-lg mx-auto py-12">
-				<div className="err-img">
-					<img src={errImg} alt="" width={300} />
-				</div>
 				<h1 class="text-6xl font-bold text-gray-800 mb-4">Oops! Something broke...</h1>
-				<p class="text-xl text-gray-600 mb-8">{error?.message || "An error occurred while loading data."}</p>
+				<p class="text-xl text-red-600 mb-8">{error?.message || "An error occurred while loading data."}</p>
 
 				<button className='refresh-button'
 					onClick={() => navigate(location.pathname, {replace: true})}
 				>Try Refreshing...</button>
+
+				<div>Or <Link to={'/'}>Go home</Link></div>
 			</div>
 		</div>
 	);
