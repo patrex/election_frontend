4b9153851f41e373ecda14ebbc26cf8f17072a18
sokari <patsoks.sokari@gmail.com>
2025-08-23 20:54:41 +0100

    add background and other styles to side menu

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -114,7 +114,7 @@ function Dashboard() {
 														<li className='side-list-item' onClick={ () => copyLink(election._id)}><i class="bi bi-cursor-fill side-menu-icon"></i>Copy Id</li>
 														<li className='side-list-item' onClick={ () => copyLink(election.shareLink)}><i class="bi bi-link-45deg side-menu-icon"></i>Copy link</li>
 														<li className='side-list-item' disabled={new Date(election.startDate) < Date.now()} onClick={ () => navigate(`/user/${params.userId}/election/${election._id}/update`)}><i class="bi bi-pencil-fill side-menu-icon"></i>Edit</li>
-														<li className='side-list-item' style={"color: red"} onClick={ () => removeElection(election)}><i class="bi bi-trash3-fill side-menu-icon"></i>Delete</li>
+														<li className='side-list-item' style={{color: "red"}} onClick={ () => removeElection(election)}><i class="bi bi-trash3-fill side-menu-icon"></i>Delete</li>
 													</ul>
 												</div>
 											}
