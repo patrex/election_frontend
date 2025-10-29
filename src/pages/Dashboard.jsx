5191d2e71d492a8d1c34d6b7b5e96f3747fa1fee
sokari <patsoks.sokari@gmail.com>
2025-10-29 07:32:29 +0100

    debug: small refactor

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,8 +11,9 @@ import { useQuery } from "@tanstack/react-query";
 import { fetcher } from '@/utils/fetcher';
 
 async function fetchElections(userId) {
-	const res = await fetcher.get(`elections/${userId}`)
-		
+	const res = await fetch(`${backendUrl}/elections/${userId}`)
+
+	if (!res.ok) throw new Error("Network error");
 	const elections = await res.json()
   	return elections;
 }
