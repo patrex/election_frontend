1f0c18bccd53bb1d9aec5444719f953a26c24c53
patrex <patsoks.sokari@gmail.com>
2025-11-20 17:29:13 +0100

    refactor Home: now if election is found and pending, show modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -409,32 +409,30 @@ function Home() {
 									<p className="text-gray-600">
 										Click <Link to={``}>here</Link> to register as a candidate.
 									</p>
-									
 								</div>
 							</div>
-						</div>
-						<div className="mt-4 flex items-start p-3 bg-gray-100 rounded-lg text-gray-600 text-xs">
-							{/* Information Icon (Inline SVG) */}
-							<svg 
-								xmlns="http://www.w3.org/2000/svg" 
-								className="h-4 w-4 flex-shrink-0 mr-2 text-indigo-500" 
-								fill="none" 
-								viewBox="0 0 24 24" 
-								stroke="currentColor" 
-								strokeWidth="2"
-							>
-								<path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-							</svg>
-							
-							{/* Footer Text Content */}
-							<p>
-								**Important:** Registering here does not imply you'll get shortlisted. The admin still needs to approve your application
-							</p>
-						</div>
-							
-						{/* footer buttons */}
-						<div className="action-btn-container">
-							<button className='Button violet action-item' onClick={ ()=> setOpenOptionsModal(false) }>Ok</button>
+							<div className="mt-4 flex items-start p-3 bg-gray-100 rounded-lg text-gray-600 text-xs">
+								{/* Information Icon (Inline SVG) */}
+								<svg 
+									xmlns="http://www.w3.org/2000/svg" 
+									className="h-4 w-4 flex-shrink-0 mr-2 text-indigo-500" 
+									fill="none" 
+									viewBox="0 0 24 24" 
+									stroke="currentColor" 
+									strokeWidth="2"
+								>
+									<path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+								</svg>
+								
+								{/* Footer Text Content */}
+								<p>
+									**Important:** Registering here does not imply you'll get shortlisted. The admin still needs to approve your application
+								</p>
+							</div>
+							{/* footer buttons */}
+							<div className="action-btn-container">
+								<button className='Button violet action-item' onClick={ ()=> setOpenOptionsModal(false) }>Ok</button>
+							</div>
 						</div>
 					</div>
 			)}
