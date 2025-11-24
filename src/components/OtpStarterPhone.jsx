0143e4394dcf75cfd86ede46c28eb2b3a29f6ca7
sokari <patsoks.sokari@gmail.com>
2025-11-24 22:44:11 +0100

    testing out modal

--- src/components/OtpStarterPhone.jsx
+++ src/components/OtpStarterPhone.jsx
@@ -25,6 +25,15 @@ const OTPStarterPhone = () => {
 
 	return (
 		<div className="w-11/12 sm:w-4/5 md:w-3/5 lg:w-2/5 xl:w-1/3 p-4 rounded-lg shadow-md relative bg-white z-100">
+			<button
+				onClick=''
+				className="absolute top-4 right-4 text-white hover:text-indigo-200 transition p-1 z-20"
+				aria-label="Close modal"
+			>
+				<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+					<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+				</svg>
+			</button>
 			<h2 className="text-3xl font-bold text-gray-800">Phone Verification</h2>
 			<p className="text-gray-600">Enter your phone number to receive a verification code.</p>
 
@@ -47,26 +56,28 @@ const OTPStarterPhone = () => {
 				</div>
 			)}
 
-			<button
-				onClick={() => {
-					handleStartClick(phoneNumber);
-					setIsLoading(true)
-				}}
-				disabled={isLoading || phoneNumber.length < 10}
-				className="w-full flex items-center justify-center py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 transition duration-200 disabled:bg-indigo-400"
-			>
-				{isLoading ? (
-					<>
-						<Loader2 className="mr-2 h-5 w-5 animate-spin" />
-						Sending OTP...
-					</>
-				) : (
-					'Request OTP'
-				)}
-			</button>
-			<p className="text-xs text-gray-500 text-center">
-				*Standard SMS rates may apply. Use country code without '+'.
-			</p>
+			<div className="w-full flex items-center justify-center">
+				<button
+					onClick={() => {
+						handleStartClick(phoneNumber);
+						setIsLoading(true)
+					}}
+					disabled={isLoading || phoneNumber.length < 10}
+					className="w-full flex items-center justify-center py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 transition duration-200 disabled:bg-indigo-400"
+				>
+					{isLoading ? (
+						<>
+							<Loader2 className="mr-2 h-5 w-5 animate-spin" />
+							Sending OTP...
+						</>
+					) : (
+						'Request OTP'
+					)}
+				</button>
+				<p className="text-xs text-gray-500 text-center">
+					*Standard SMS rates may apply. Use country code without '+'.
+				</p>
+			</div>
 		</div>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -201,15 +201,7 @@ function Home() {
 			{otpStarterModal && (
 				<div className="modal-overlay">
 					<>
-						<button
-							onClick={() => setOtpStarterModal(false)}
-							className="absolute top-4 right-4 text-white hover:text-indigo-200 transition p-1 z-20"
-							aria-label="Close modal"
-						>
-							<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-								<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-							</svg>
-						</button>
+						
 						<OTPStarterPhone />
 					</>
 				</div>
