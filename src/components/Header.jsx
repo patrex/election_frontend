ace2e0eb899e955ccda991f57c3a8beb0a469d3c
patrex <patsoks.sokari@gmail.com>
2026-03-20 17:18:40 +0100

    replace AppContext everywhere

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
-import { AppContext } from "@/App";
-import { useContext } from "react";
 import Navbar from "./Navbar";
 import Toast from "@/utils/ToastMsg";
+
+import { useAuth } from "@/contexts/AuthContext";
 
 import { signOut } from "firebase/auth";
 import { authman } from "@/utils/fireloader";
 
 function Header() {
-	const { user } = useContext(AppContext);
+	const { logout } = useAuth();
 
 	async function logout(){
 		try {
-			await signOut(authman);
+			await logout();
 		} catch (error) {
 			Toast.error("There was an error logging you out");
 		}
