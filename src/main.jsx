5cdf68e540b7aace787f2e0df33e56d744ae6dea
patrex <patsoks.sokari@gmail.com>
2026-06-05 16:43:08 +0100

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import './index.css'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import { configureFetcher } from './utils/fetcher';
-import Toast from './utils/ToastMsg.js'
 import axios from 'axios'
 
 axios.defaults.withCredentials=true;
@@ -14,25 +12,6 @@ axios.defaults.baseURL='/api';
 
 import { ElectionProvider } from './contexts/ElectionContext';
 import { OTPProvider } from './contexts/OTPContext.jsx'
-
-// Configure once at app startup
-configureFetcher({
-	baseURL: '/api', // or process.env.REACT_APP_BACKEND_URL
-	onAuthError: (error) => {
-	    if (error.code === 'AUTH_REQUIRED') {
-		Toast.error('Please log in to continue');
-		// Optionally redirect to login
-		window.location.href = '/login';
-	    } else if (error.code === 'TOKEN_EXPIRED') {
-		Toast.error('Your session has expired. Please log in again.');
-		// Optionally redirect to login
-		window.location.href = '/login';
-	    }
-	},
-	onNetworkError: (error) => {
-	    Toast.error('Network error. Please check your connection.');
-	}
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
